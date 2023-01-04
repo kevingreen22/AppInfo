311dcd21ef4a3c5c12fb6d9761aeb592ee2cf734
Kevin Green <stuckbykev@gmail.com>
2023-01-04 13:01:27 -0800

    Initial Commit

---
+++ Sources/AppInfo/AppInfo.swift
@@ -0,0 +1,48 @@
+//
+//  AppInfo.swift
+//  Special Order Keeper iPhone
+//
+//  Created by LukeSideWalker (StackOverflow) on 2/23/20.
+//  https://stackoverflow.com/questions/24501288/getting-version-and-build-information-with-swift
+
+import Foundation
+
+struct AppInfo {
+    
+    // Holds a reference to the Info.plist of the app as a Dictionary
+    static private let infoPlistDictionary = Bundle.main.infoDictionary
+    
+    /// Retrieves and returns associated values (of Type String) from info.Plist of the app.
+    static private func readFromInfoPlist(withKey key: String) -> String? {
+        return infoPlistDictionary?[key] as? String
+    }
+    
+    static var appName: String {
+        return readFromInfoPlist(withKey: "CFBundleName") ?? "(unknown app name)"
+    }
+    
+    static var version: String {
+        return readFromInfoPlist(withKey: "CFBundleShortVersionString") ?? "(unknown app version)"
+    }
+    
+    static var build: String {
+        return readFromInfoPlist(withKey: "CFBundleVersion") ?? "(unknown build number)"
+    }
+    
+    static var minimumOSVersion: String {
+        return readFromInfoPlist(withKey: "MinimumOSVersion") ?? "(unknown minimum OSVersion)"
+    }
+    
+    static var copyrightNotice: String {
+        return readFromInfoPlist(withKey: "NSHumanReadableCopyright") ?? "(unknown copyright notice)"
+    }
+    
+    static var bundleIdentifier: String {
+        return readFromInfoPlist(withKey: "CFBundleIdentifier") ?? "(unknown bundle identifier)"
+    }
+    
+    static var developer: String {
+        return "Kevin Green"
+    }
+    
+}
